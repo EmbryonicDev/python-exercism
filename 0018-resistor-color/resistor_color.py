d4b5ea8f3a6ad357caf9ad65afccd85e0ddefdfb
DJ de Vlamingh <Embryonic.dev@gmail.com>
2023-06-19 18:03:01 +0200

    Complete exercise: resistor color
    ~

--- 0018-resistor-color/resistor_color.py
+++ 0018-resistor-color/resistor_color.py
@@ -1,6 +1,24 @@
-def color_code(color):
-    pass
+def return_color_codes():
+    return {
+        'black': 0,
+        'brown': 1,
+        'red': 2,
+        'orange': 3,
+        'yellow': 4,
+        'green': 5,
+        'blue': 6,
+        'violet': 7,
+        'grey': 8,
+        'white': 9,
+        }
+    
 
+def color_code(color):
+    return return_color_codes()[color]
 
 def colors():
-    pass
+    return [k for k in return_color_codes()]
+
+
+print(color_code('yellow'))
+print(colors())
