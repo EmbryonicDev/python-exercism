d1eeb03bc75be66be735a68da29f20a49bbb86ac
DJ de Vlamingh <Embryonic.dev@gmail.com>
2023-06-21 11:56:44 +0200

    Complete Exercise: Isogram

--- 0023-isogram/isogram.py
+++ 0023-isogram/isogram.py
@@ -1,2 +1,21 @@
 def is_isogram(string):
-    pass
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    word_check = ''
+    for l in string:
+        l_lower = l.lower()
+        if l_lower in word_check:
+            return False
+        if l_lower in alpha:
+            word_check += l_lower
+    return True
+    
+    
+print(is_isogram('lumberjacks'))
+print(is_isogram('lumberjackss'))
+print(is_isogram('background'))
+print(is_isogram('backgrounnd'))
+print(is_isogram('downstream'))
+print(is_isogram('downstreamm'))
+print(is_isogram('six-year-old'))
+print(is_isogram('six-yeear-old'))
+
