5e21532ce220cc490f50800831eea9f67ac3855d
DJ de Vlamingh <Embryonic.dev@gmail.com>
2023-06-22 14:24:34 +0200

    Complete exercise: armstrong numbers

--- 0028-armstrong-numbers/armstrong_numbers.py
+++ 0028-armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,25 @@
 def is_armstrong_number(number):
-    pass
+    str_num = str(number)
+    num_len = len(str_num)
+    check = 0
+    
+    for num in str_num:
+        num = int(num)
+        check += num ** num_len
+        
+    return check == number
+
+    
+    
+    
+print(is_armstrong_number(5))
+print(is_armstrong_number(10))
+print(is_armstrong_number(153))
+print(is_armstrong_number(9474))
+print(is_armstrong_number(9475))
+
+
+
+
+
+
