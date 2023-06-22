8222dff204f71e5382ed9f8d7fcc5b63187a53a4
DJ de Vlamingh <Embryonic.dev@gmail.com>
2023-06-22 14:15:16 +0200

    Complete exercise: rna transcription

--- 0027-rna-transcription/rna_transcription.py
+++ 0027-rna-transcription/rna_transcription.py
@@ -1,2 +1,22 @@
 def to_rna(dna_strand):
-    pass
+    dna_compliments = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U",
+   }
+    rna = ''
+    for l in dna_strand:
+        rna += dna_compliments[l]
+    return rna
+
+print(to_rna(''))
+print(to_rna('C'))
+print(to_rna('G'))
+print(to_rna('T'))
+print(to_rna('A'))
+print(to_rna('ACGTGGTCTTAA'))
+
+
+
+
