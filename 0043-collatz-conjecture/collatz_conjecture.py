ad00d6043e90b2646e5a3ea49d31b291e5824974
djdevlam-apple <embryonic.dev@gmail.com>
2023-08-09 12:44:26 +0200

    Complete exercise: collatz_conjecture

--- 0043-collatz-conjecture/collatz_conjecture.py
+++ 0043-collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,29 @@
+"""
+The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+
+Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
+
+Given a number n, return the number of steps required to reach 1.
+"""
+
+
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    total_steps = 0
+    while number != 1:
+        if number % 2 == 0:
+            number //= 2
+        else:
+            number = (number * 3) + 1
+        total_steps += 1
+
+    return total_steps
+
+
+if __name__ == "__main__":
+    print(steps(12))
+    # print(steps(-12))
+    print(steps(1))
+    print(steps(16))
