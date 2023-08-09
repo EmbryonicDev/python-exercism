c345b7177abbc405bd78f15fce7c4cee3b424e1c
djdevlam-apple <embryonic.dev@gmail.com>
2023-08-09 18:22:09 +0200

    Complete exercise: secret handshake

--- 0044-secret-handshake/secret_handshake.py
+++ 0044-secret-handshake/secret_handshake.py
@@ -1,2 +1,54 @@
 def commands(binary_str):
-    pass
+    actions = [
+        "jump",
+        "close your eyes",
+        "double blink",
+        "wink",
+    ]
+
+    # Remove binary_str[0] to get action digits
+    binary_str_copy = binary_str[1:]
+
+    # Get actions list
+    handshake_actions = [
+        actions[i] for i in range(3, -1, -1) if binary_str_copy[i] == "1"
+    ]
+
+    # Reverse if required
+    if binary_str[0] == "1":
+        return handshake_actions[::-1]
+
+    return handshake_actions
+
+
+"""
+00001 = "wink"
+00010 = "double blink"
+00100 = "close your eyes"
+01000 = "jump"
+10000 = Reverse the order of the operations in the secret handshake.
+"""
+
+
+if __name__ == "__main__":
+    print(commands("00001"), ["wink"])
+
+    print(commands("00010"), ["double blink"])
+
+    print(commands("00100"), ["close your eyes"])
+
+    print(commands("01000"), ["jump"])
+
+    print(commands("00011"), ["wink", "double blink"])
+
+    print(commands("10011"), ["double blink", "wink"])
+
+    print(commands("11000"), ["jump"])
+
+    print(commands("10000"), [])
+
+    print(commands("01111"), ["wink", "double blink", "close your eyes", "jump"])
+
+    print(commands("11111"), ["jump", "close your eyes", "double blink", "wink"])
+
+    print(commands("00000"), [])
