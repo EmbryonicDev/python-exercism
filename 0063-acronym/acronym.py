c7b699c535e455431ec91906a60eb2fa656e4ecd
de Vlamingh, Dewald <dj.de.vlamingh@oracle.com>
2023-11-28 10:58:03 +0100

    Complete exercise: acronym

--- 0063-acronym/acronym.py
+++ 0063-acronym/acronym.py
@@ -1,2 +1,32 @@
 def abbreviate(words):
-    pass
+    # Split the words by spaces, hyphens, and underscores
+    acronym = "".join(
+        word[0].upper()
+        for word in words.replace("-", " ").replace("_", " ").split()
+        if word[0].isalnum()
+    )
+    return acronym
+
+if __name__ == "__main__":
+    print(abbreviate("Portable Network Graphics"), "PNG")
+
+    print(abbreviate("Ruby on Rails"), "ROR")
+
+    print(abbreviate("First In, First Out"), "FIFO")
+
+    print(abbreviate("GNU Image Manipulation Program"), "GIMP")
+
+    print(abbreviate("Complementary metal-oxide semiconductor"), "CMOS")
+
+    print(
+        abbreviate(
+            "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"
+        ),
+        "ROTFLSHTMDCOALM",
+    )
+
+    print(abbreviate("Something - I made up from thin air"), "SIMUFTA")
+
+    print(abbreviate("Halley's Comet"), "HC")
+
+    print(abbreviate("The Road _Not_ Taken"), "TRNT")
