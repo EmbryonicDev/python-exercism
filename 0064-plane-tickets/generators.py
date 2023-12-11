562f53cf136f900ac416049151aa1a34288c9782
de Vlamingh, Dewald <dj.de.vlamingh@oracle.com>
2023-12-11 10:46:44 +0100

    Complete exercise: plane tickets Generators

--- 0064-plane-tickets/generators.py
+++ 0064-plane-tickets/generators.py
@@ -11,11 +11,9 @@ def generate_seat_letters(number):
     After D it should start again with A.
 
     Example: A, B, C, D
-
     """
-
-    pass
-
+    for i in range(number):
+        yield "ABCD"[i % 4]
 
 def generate_seats(number):
     """Generate a series of identifiers for airline seats.
@@ -33,8 +31,17 @@ def generate_seats(number):
     Example: 3C, 3D, 4A, 4B
 
     """
+    num = 1
+    count = 0
+    seat_letters = generate_seat_letters(number)
+    for i in range(number):
+        yield f"{num}{next(seat_letters)}"
+        count += 1
+        if count % 4 == 0:
+            num += 1
+            if num == 13:
+                num += 1
 
-    pass
 
 def assign_seats(passengers):
     """Assign seats to passengers.
@@ -45,8 +52,12 @@ def assign_seats(passengers):
     Example output: {"Adele": "1A", "Björk": "1B"}
 
     """
+    passengers_dict = {}
+    seat_numbers = generate_seats(len(passengers))
+    for passenger in passengers:
+        passengers_dict[passenger] = next(seat_numbers)
 
-    pass
+    return passengers_dict
 
 def generate_codes(seat_numbers, flight_id):
     """Generate codes for a ticket.
@@ -56,5 +67,7 @@ def generate_codes(seat_numbers, flight_id):
     :return: generator - generator that yields 12 character long ticket codes.
 
     """
-
-    pass
+    required_len = 12
+    for num in seat_numbers:
+        # ljust adds chars to end, rjust adds chars to the start
+        yield f'{num}{flight_id}'.ljust(required_len, '0')
