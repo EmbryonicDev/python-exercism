0bcf9020352a1fd337000ee95c7a11f6bcca03ad
DJ de Vlamingh <Embryonic.dev@gmail.com>
2023-12-18 12:01:21 +0100

    Complete exercise: nth prime

--- 0067-nth-prime/nth_prime.py
+++ 0067-nth-prime/nth_prime.py
@@ -1,2 +1,13 @@
 def prime(number):
-    pass
+    if number < 1:
+        raise ValueError('there is no zeroth prime')
+
+    primes = [2]
+    current_num = 3
+
+    while len(primes) < number:
+        if all(current_num % num != 0 for num in primes):
+            primes.append(current_num)
+        current_num += 2  # Skip even numbers (except 2) to improve efficiency
+
+    return primes[-1]
