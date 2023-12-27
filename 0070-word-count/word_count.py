3879619df2fbbe85154972a4bebcf503a652968a
DJ de Vlamingh <Embryonic.dev@gmail.com>
2023-12-27 09:29:13 +0100

    Complete exercise: word count

--- 0070-word-count/word_count.py
+++ 0070-word-count/word_count.py
@@ -1,2 +1,15 @@
+import re
+
+
 def count_words(sentence):
-    pass
+    word_counts = {}
+
+    # Replace non-alphanumeric characters (except apostrophes)
+    # with spaces & convert sentence to lowercase
+    sentence = re.sub(r"[^a-z0-9'\s]", " ", sentence.lower())
+
+    # Count word occurrences
+    for word in re.findall(r"\b\w+(?:'\w+)*(?:\b|$)", sentence):
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+    return word_counts
