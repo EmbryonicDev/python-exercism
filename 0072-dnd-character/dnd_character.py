bf44a4e4411cde461821f1d9ef9a7ff79422b2b7
DJ de Vlamingh <Embryonic.dev@gmail.com>
2024-01-10 11:34:06 +0100

    Complete exercise: dnd character

--- 0072-dnd-character/dnd_character.py
+++ 0072-dnd-character/dnd_character.py
@@ -1,3 +1,24 @@
+import random
+
+
+def modifier(score):
+    return (score - 10) // 2
+
+
 class Character:
     def __init__(self):
-        pass
+        self.abilities = [self.generate_ability() for _ in range(6)]
+        (
+            self.strength, self.dexterity, self.constitution, self.intelligence,
+            self.wisdom, self.charisma
+        ) = self.abilities
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    @staticmethod
+    def generate_ability():
+        dice_rolls = [random.randint(1, 6) for _ in range(4)]
+        dice_rolls.remove(min(dice_rolls))
+        return sum(dice_rolls)
+
+    def ability(self):
+        return random.choice(self.abilities)
